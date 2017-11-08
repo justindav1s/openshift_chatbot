8cff298e7bb3044c49fe1e19ccfa30505ce78739
justindav1s <justin.davis@ba.com>
2017-11-08 20:00:48 +0000

    s2i config

---
+++ bin/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export IP=127.0.0.1
+export USER=justin
+export PROJECT=tensorflow2
+export IMAGE_NAMESPACE=$PROJECT
+export IMAGE=tfchatbot
+export REGISTRY_HOST=docker-registry-default.apps.${IP}.xip.io
+export REGISTRY_HOST=172.30.1.1:5000
+
+oc login ${IP}:8443 -u system:admin
+oc adm policy add-cluster-role-to-user cluster-admin $USER
+
+oc login ${IP}:8443 -u $USER
+
+oc new-project ${PROJECT}
+
+oc project $PROJECT
+
+oc new-app centos/python-35-centos7~https://github.com/justindav1s/openshift_chatbot.git
